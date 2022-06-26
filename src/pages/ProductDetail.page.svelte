6c874262f8494e14bc9d50f7db379a9dff0c1804
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2022-06-26 23:47:29 +0700

    feat: add ProductDetailPage

---
+++ src/pages/ProductDetail.page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import {fetchProductById} from '../store/product';
+
+  export let productId: number;
+
+  const product = fetchProductById(productId);
+</script>
+
+<div>{JSON.stringify($product)}</div>
