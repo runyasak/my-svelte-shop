bc199826cb3528462364a4750d40c4aaa54678b9
Runyasak Chaengnaimuang <runyasak.c@gmail.com>
2022-08-17 22:49:58 +0700

    styles: implement card and button

--- src/pages/Home.page.svelte
+++ src/pages/Home.page.svelte
@@ -6,14 +6,16 @@
 {#if $fetchProducts}
   <div class="flex gap-8 flex-wrap justify-center w-full">
     {#each $fetchProducts as product}
-      <div class="shadow-lg rounded-lg w-full max-w-[16em] flex flex-col">
+      <div
+        class="card shadow-lg rounded-lg w-full max-w-[16em] flex flex-col bg-white"
+      >
         <div class="flex flex-col gap-4 p-8">
           <div
             class="w-full min-h-[10em] bg-contain bg-no-repeat bg-center"
             style="background-image: url('{product.image}')"
           />
           <div class="flex flex-col gap-4">
-            <span class="text-[#15202B] font-bold text-xl text-center">
+            <span class=" font-bold text-xl text-center">
               {product.title}
             </span>
             <span class="text-teal-500 text-center text-xl">
@@ -24,7 +26,7 @@
 
         <Link
           to={`/products/${product.id}`}
-          class="mt-auto bg-teal-500 text-white py-4 rounded-b-lg text-center"
+          class="btn btn-primary mt-auto rounded-b-lg rounded-t-none text-center"
         >
           ADD TO CART
         </Link>
